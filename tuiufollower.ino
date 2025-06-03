92334a5280a4403ee18a5592e6b71191313633ac
darcihp <32653697+darcihp@users.noreply.github.com>
2025-06-03 17:12:44 -0300

    Basic Read array

--- tuiufollower.ino
+++ tuiufollower.ino
@@ -1,6 +1,10 @@
-#include <driver/adc.h>
+/*
+|   |   |   |   |   |   |   |   |
 
-#define ir 21
+
+*/
+
+
 
 //H-bridge
 #define a1a 5
@@ -8,63 +12,145 @@
 #define b1a 19
 #define b1b 18
 
-#define DEBUG 0
+#define DEBUG 2
+
+#define sensorOffset 300
+
+int r_d1, r_d2, r_d3, r_d4, r_d5, r_d6, r_d7, r_d8;
+int c_r_d1, c_r_d2, c_r_d3, c_r_d4, c_r_d5, c_r_d6, c_r_d7, c_r_d8;
 
 void setup() {
 
   Serial.begin(115200);
 
-  adc1_config_width(ADC_WIDTH_BIT_12);
-
-  //Pin 32
-  adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_11);
-  //Pin 33
-  adc1_config_channel_atten(ADC1_CHANNEL_5, ADC_ATTEN_DB_11);
-
-  //Pin 13
-  adc2_config_channel_atten(ADC2_CHANNEL_4, ADC_ATTEN_DB_11);
-  //Pin 12
-  adc2_config_channel_atten(ADC2_CHANNEL_5, ADC_ATTEN_DB_11);
-  //Pin 14
-  adc2_config_channel_atten(ADC2_CHANNEL_6, ADC_ATTEN_DB_11);
-  //Pin 27
-  adc2_config_channel_atten(ADC2_CHANNEL_7, ADC_ATTEN_DB_11);
-  //Pin 26
-  adc2_config_channel_atten(ADC2_CHANNEL_9, ADC_ATTEN_DB_11);
-  //Pin 25
-  adc2_config_channel_atten(ADC2_CHANNEL_8, ADC_ATTEN_DB_11);
-
-  pinMode(ir, OUTPUT);
-  digitalWrite(ir, HIGH);
-
   pinMode(a1a, OUTPUT);
   pinMode(a1b, OUTPUT);
   pinMode(b1a, OUTPUT);
   pinMode(b1b, OUTPUT);
+
+  calibration();
+}
+
+void calibration() {
+  c_r_d1 = 0;
+  c_r_d2 = 0;
+  c_r_d3 = 0;
+  c_r_d4 = 0;
+  c_r_d5 = 0;
+  c_r_d6 = 0;
+  c_r_d7 = 0;
+  c_r_d8 = 0;
+
+  for (int i = 0; i < 100; i++) {
+    arrayRead();
+    c_r_d1 = c_r_d1 + r_d1;
+    c_r_d2 = c_r_d2 + r_d2;
+    c_r_d3 = c_r_d3 + r_d3;
+    c_r_d4 = c_r_d4 + r_d4;
+    c_r_d5 = c_r_d5 + r_d5;
+    c_r_d6 = c_r_d6 + r_d6;
+    c_r_d7 = c_r_d7 + r_d7;
+    c_r_d8 = c_r_d8 + r_d8;
+  }
+
+  c_r_d1 = c_r_d1 / 100;
+  c_r_d2 = c_r_d2 / 100;
+  c_r_d3 = c_r_d3 / 100;
+  c_r_d4 = c_r_d4 / 100;
+  c_r_d5 = c_r_d5 / 100;
+  c_r_d6 = c_r_d6 / 100;
+  c_r_d7 = c_r_d7 / 100;
+  c_r_d8 = c_r_d8 / 100;
+
+  c_r_d1 = c_r_d1 + sensorOffset;
+  c_r_d2 = c_r_d2 + sensorOffset;
+  c_r_d3 = c_r_d3 + sensorOffset;
+  c_r_d4 = c_r_d4 + sensorOffset;
+  c_r_d5 = c_r_d5 + sensorOffset;
+  c_r_d6 = c_r_d6 + sensorOffset;
+  c_r_d7 = c_r_d7 + sensorOffset;
+  c_r_d8 = c_r_d8 + sensorOffset;
+
+#if DEBUG == 2
+  Serial.println("CALIBRATION VALUES");
+  Serial.print(c_r_d1);
+  Serial.print(" - ");
+  Serial.print(c_r_d2);
+  Serial.print(" - ");
+  Serial.print(c_r_d3);
+  Serial.print(" - ");
+  Serial.print(c_r_d4);
+  Serial.print(" - ");
+  Serial.print(c_r_d5);
+  Serial.print(" - ");
+  Serial.print(c_r_d6);
+  Serial.print(" - ");
+  Serial.print(c_r_d7);
+  Serial.print(" - ");
+  Serial.print(c_r_d8);
+  Serial.println(" - ");
+
+#endif
+}
+
+void arrayRead() {
+  r_d1 = analogRead(13);
+  delay(1);
+  r_d2 = analogRead(12);
+  delay(1);
+  r_d3 = analogRead(14);
+  delay(1);
+  r_d4 = analogRead(27);
+  delay(1);
+  r_d5 = analogRead(26);
+  delay(1);
+  r_d6 = analogRead(25);
+  delay(1);
+  r_d7 = analogRead(33);
+  delay(1);
+  r_d8 = analogRead(32);
+  delay(1);
+}
+
+void moveMotors(int _a, int _b) {
+  analogWrite(a1a, _a);
+  analogWrite(a1b, 255 - _a);
+  analogWrite(b1a, _b);
+  analogWrite(b1b, 255 - _b);
+}
+
+int calculatePosition() {
+  int position = 0;
+
+  if (r_d1 > c_r_d1)
+    position = position + 8;
+  if (r_d2 > c_r_d2)
+    position = position + 4;
+  if (r_d3 > c_r_d3)
+    position = position + 2;
+  if (r_d4 > c_r_d4)
+    position = position + 1;
+  if (r_d5 > c_r_d5)
+    position = position - 1;
+  if (r_d6 > c_r_d6)
+    position = position - 2;
+  if (r_d7 > c_r_d7)
+    position = position - 4;
+  if (r_d8 > c_r_d8)
+    position = position - 8;
+  return position;
 }
 
 void loop() {
 
-  int r_d1, r_d2, r_d3, r_d4, r_d5, r_d6;
-  esp_err_t r = adc2_get_raw(ADC2_CHANNEL_4, ADC_WIDTH_12Bit, &r_d1);
-  r = adc2_get_raw(ADC2_CHANNEL_5, ADC_WIDTH_12Bit, &r_d2);
-  r = adc2_get_raw(ADC2_CHANNEL_6, ADC_WIDTH_12Bit, &r_d3);
-  r = adc2_get_raw(ADC2_CHANNEL_7, ADC_WIDTH_12Bit, &r_d4);
-  r = adc2_get_raw(ADC2_CHANNEL_9, ADC_WIDTH_12Bit, &r_d5);
-  r = adc2_get_raw(ADC2_CHANNEL_8, ADC_WIDTH_12Bit, &r_d6);
-  int r_d7 = adc1_get_raw(ADC1_CHANNEL_5);
-  int r_d8 = adc1_get_raw(ADC1_CHANNEL_4);
+  arrayRead();
+  Serial.println(calculatePosition());
 
+  
 
-  /*
-  digitalWrite(a1a, HIGH);
-  digitalWrite(a1b, LOW);
-  digitalWrite(b1a, HIGH);
-  digitalWrite(b1b, LOW);
-  */
+  //moveMotors(128, 128);
 
-
-  #if DEBUG == 1
+#if DEBUG == 1
   Serial.print(r_d1);
   Serial.print(" - ");
   Serial.print(r_d2);
@@ -81,5 +167,6 @@ void loop() {
   Serial.print(" - ");
   Serial.print(r_d8);
   Serial.println(" - ");
-  #endif
+
+#endif
 }
